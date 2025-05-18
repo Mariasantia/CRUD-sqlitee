04a00f4a25519627a9b9c524deb6543e1c8d4b50
mariasantia <mariashantym@gmail.com>
2025-05-18 12:02:00 +0700

    mengubah fungsi read

--- daftarsetailtugas.php
+++ daftarsetailtugas.php
@@ -1,25 +1,31 @@
 <?php
+// Asumsikan koneksi $conn sudah dibuat sebelumnya
+
+// Ambil semua tugas
 $sql = 'SELECT id, deskripsi, waktu FROM tugas';
-
 $statement = $conn->query($sql);
-$tugas = $statement->fetchAll(PDO::FETCH_ASSOC);
+$tugas_list = $statement->fetchAll(PDO::FETCH_ASSOC);
 
-if ($tugas) {
- foreach ($tugas as $t) {
-  echo $t['deskripsi'] . '<br>';
- }
+if ($tugas_list) {
+    foreach ($tugas_list as $t) {
+        echo $t['deskripsi'] . '<br>';
+    }
 }
-<?php 
+
+echo "<hr>";
+
+// Ambil tugas berdasarkan id
 $id = 1;
 $sql = 'SELECT id, deskripsi, waktu FROM tugas WHERE id = :tugas_id';
-
 $statement = $conn->prepare($sql);
 $statement->bindParam(':tugas_id', $id, PDO::PARAM_INT);
 $statement->execute();
 $tugas = $statement->fetch(PDO::FETCH_ASSOC);
 
 if ($tugas) {
- echo $tugas['id'] . '.' . $tugas['deskripsi'];
+    echo $tugas['id'] . '. ' . $tugas['deskripsi'];
 } else {
- echo "Tugas dengan id $id tidak ditemukan.";
+    echo "Tugas dengan id $id tidak ditemukan.";
 }
+?>
+
